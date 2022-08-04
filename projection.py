e5716beb9f3b64dcd928ca434e283c3de8a0d4af
Evangelia Grizaniti <grizanit@wgs30.zeuthen.desy.de>
2022-08-04 13:46:59 +0200

    added the function

--- projection.py
+++ projection.py
@@ -1,5 +1,8 @@
+from icecube.icetray import I3Units
+from icecube.dataclasses import I3Constants
+from icecube.clsim import GetIceCubeDOMAngularSensitivity
+from icecube.simclasses import I3CLSimFunctionPolynomial
 from icecube.icetray import I3Units, I3Module, traysegment
-from icecube.clsim import GetIceCubeDOMAcceptance, GetIceCubeDOMAngularSensitivity
 import numpy as np
 import matplotlib.pyplot as plt
 import argparse
@@ -12,6 +15,7 @@ parser = OptionParser(usage, description="Get the effective area of a sphere dep
 parser.add_option("--dist", dest="dist", type="float", default=None, help="distance from surface of the sphere")
 parser.add_option("--r", dest="r", type="float", default=1, help="Radius of the sphere")
 parser.add_option("--samples", dest="samples", type="int", default=100, help="explicitly declare the Δtheta while iterating for different azimuthial values")
+parser.add_option("--direction", choices = ('down','up'), dest="direction", default='down', help="direction of the IceCube DOM -- only legible options are 'down' and 'up' ")
 
 
 opts, _  = parser.parse_args()
@@ -20,12 +24,17 @@ print("Options Loaded")
 
 print("Options: {}".format(opts))
 
+
 dist = opts.dist
 r = opts.r
 samples = opts.samples
 theta = np.linspace(0,np.pi,samples)
 costheta=np.cos(theta)
+direction = opts.direction
 
+
+DOMRadius = 0.16510*I3Units.m # 13" diameter
+referenceArea = I3Constants.pi*DOMRadius**2
 
 def create_sphere(x0,y0,z0,r,samples=100):
     phi = np.linspace(0,2*np.pi,samples)
@@ -48,61 +57,78 @@ ax.set_zlim([-1,1])
 plt.show()
 """
 
-####an infinite plane at angle theta
+####find the effective area
 
-def effective(cth,r):
-    return np.pi*r**2*(1+cth)/2
+def effective(cth,r,direction='down'):
+    if direction == 'down':
+        mult=-1
+    else:
+        mult=1
+    return np.pi*r**2*(1+cth*mult)/2
 
 
 
-def find_projection(r,theta):
+def find_projection(r,theta,direction):
     total_area = np.pi*r**2
     values=np.array(theta)
     fraction=np.array(theta)
     for i, th in enumerate(theta):
-        values[i] = effective(np.cos(th),r)
+        values[i] = effective(np.cos(th),r,direction)
         #print(values[i])
         fraction[i] = values[i]/total_area
         #print(f"fraction is: {fraction[i]}" )
     return fraction,values
 
 def ang_sens(costheta):
-#angular_sensitivity is a function
+    #angular_sensitivity is a function
     angular_sensitivity = GetIceCubeDOMAngularSensitivity()
-    sensitivity = angular_sensitivity.GetValue(1)
-    print(sensitivity,angular_sensitivity.GetCoefficients())
+    #sensitivity = angular_sensitivity.GetValue(1)
+    #print(sensitivity,angular_sensitivity.GetCoefficients())
 
     #pass the cos(theta) you want
     getValues = np.vectorize(angular_sensitivity.GetValue)
     sensitivity = np.array(costheta)
+    #saving values in reverse order
     for i, costh in enumerate(costheta):
-        sensitivity[i] = getValues(float(costh))
+        sensitivity[len(costheta)-1-i] = getValues(float(costh))
     return sensitivity
 
-fraction,values = find_projection(r,theta)
+fraction,values = find_projection(r,theta,direction)
 sensitivity = ang_sens(costheta)
 maxsensitivity=max(sensitivity)
 fraction *= maxsensitivity
 
 
+def plot_methods(fraction,sensitivity,costheta):
+    fig,axes = plt.subplots(3,figsize=(10,10))
+    #fig.canvas.set_window_title("Projection of a half-sphere at a certain zenith")
+    axes[0].xlim=([-1,1])
+    axes[0].ylim=([min(fraction[0],min(sensitivity)),max(fraction[samples-1],max(sensitivity))])
+    axes[0].xlim=([-1,1])
+    axes[0].ylim=([min(fraction[0],min(sensitivity)),max(fraction[samples-1],max(sensitivity))])
+    axes[0].set_xlabel("$\\cos$$\\theta$")
+    axes[0].set_ylabel("Fraction of Area Visible")
+    axes[1].set_xlabel("$\\theta$ [radius]")
+    axes[1].set_ylabel("Fraction of Area Visible")
+    #axes[0].set_title("Projection of a half-sphere at a certain zenith")
+    axes[1].plot(costheta,fraction, '-r',label='geometric method')
+    axes[0].plot(costheta, sensitivity,'-g',label='dom default sensitivity')
+    axes[0].legend()
+    axes[1].legend()
+    axes[2].xlim=([-1,1])
+    axes[2].ylim=([min(fraction[0],min(sensitivity)),max(fraction[samples-1],max(sensitivity))])
+    axes[2].plot(costheta,sensitivity-fraction,'-c')
+    fig.savefig("projection.png")
 
-fig,axes = plt.subplots(3,figsize=(8,8))
-#fig.canvas.set_window_title("Projection of a half-sphere at a certain zenith")
-axes[0].xlim=([-1,1])
-axes[0].ylim=([min(fraction[0],min(sensitivity)),max(fraction[samples-1],max(sensitivity))])
-axes[0].xlim=([-1,1])
-axes[0].ylim=([min(fraction[0],min(sensitivity)),max(fraction[samples-1],max(sensitivity))])
-axes[0].set_xlabel("$\\cos$$\\theta$")
-axes[0].set_ylabel("Fraction of Area Visible")
-axes[1].set_xlabel("$\\theta$ [radius]")
-axes[1].set_ylabel("Fraction of Area Visible")
-#axes[0].set_title("Projection of a half-sphere at a certain zenith")
-axes[1].plot(costheta,fraction, '-r',label='geometric method')
-axes[0].plot(costheta, sensitivity,'-g',label='dom default sensitivity')
-axes[0].legend()
-axes[1].legend()
-axes[2].xlim=([-1,1])
-axes[2].ylim=([min(fraction[0],min(sensitivity)),max(fraction[samples-1],max(sensitivity))])
-axes[2].plot(costheta,sensitivity-fraction,'-c')
-fig.savefig("projection.png")
+plot_methods(fraction,sensitivity,costheta)
 
+
+def GetIceCubeDOMAngularSensitivityLinear(direction):
+    if(direction == 'down'):
+        mult=-1
+    else:
+        mult=+1
+    coefficients = [1/2,1/2*mult]
+    return I3CLSimFunctionPolynomial(coefficients)
+
+print(GetIceCubeDOMAngularSensitivityLinear(direction))
